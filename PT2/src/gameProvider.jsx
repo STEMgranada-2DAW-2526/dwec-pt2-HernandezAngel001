5ce246030a766bf4c915033fb466c7712b51b91f
AngelHernandezzZ <angelhernandez001.alu@stemgranada.com>
2025-11-25 11:28:25 +0100

    wip gamePreovider

--- PT2/src/gameProvider.jsx
+++ PT2/src/gameProvider.jsx
@@ -13,7 +13,7 @@ const estadoInicial = {
     { id: "renos", etiqueta: "Renos-Lanzamisiles", dano: 5, costo: 30 },
     { id: "arbol", etiqueta: "Árbol de Navidad Láser", dano: 10, costo: 50 },
   ],
-};
+}
 
 function reducerJuego(estado, accion) {
   switch (accion.tipo) {
@@ -21,10 +21,20 @@ function reducerJuego(estado, accion) {
       return {
         ...estado,
         danoRealizado: estado.danoRealizado + estado.danoPorDisparo,
-      };
+      }
     case "DISPARO_AUTO":
       return {
         ...estado,
         danoRealizado: estado.danoRealizado + estado.danoPorDisparo * estado.disparosAutoPorSegundo,
-      };
-    case "COMP":
+      }
+    case "COMPRAR_MULTIPLICADOR":
+      if (estado.caramelos < estado.costoMultiplicador) return estado
+      return {
+        ...estado,
+        caramelos: estado.caramelos - estado.costoMultiplicador,
+        disparosAutoPorSegundo: estado.disparosAutoPorSegundo + 1,
+        costoMultiplicador: Math.ceil(estado.costoMultiplicador * 1.2),
+      }
+    case "":
+    }
+}
